030f2b562ca883976be60d721bd955bbeac8d47f
humayunadilshahzad <humayun_adil@hotmail.com>
2023-10-11 00:18:26 +0200

    recalling components

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -4,5 +4,6 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <h3 className={styles.title}>Bismillah</h3>
+    <button onclick> click Me</button>
      );
 }
